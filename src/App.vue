41e5291f168daa8d53cbf33ddf9125f028ae00c1
gilphilbert <gil.philbert@gmail.com>
2021-05-26 15:43:11 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div#app
+    nav#menubar
+    h1 Wine Sense 
+    PersonGreeter(personName='Phill')
+</template>
+<script>
+import PersonGreeter from './components/PersonGreeter.vue'
+export default {
+  name: 'App',
+  components: {
+    PersonGreeter
+  }
+}
+</script>
+<style>
+html {
+  background-color: #EBEEF3;
+  height: 100%;
+}
+body {
+  height: 100%;
+}
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+#app #menubar {
+  display: flex;
+  width: 300px;
+  height: 100%;
+  background-color: #354052;
+}
+</style>
---
+++ src/components/PersonGreeter.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  h2 Hello, {{ personName }}!
+</template>
+<script>
+module.exports = {
+  name: 'PersonGreeter',
+  props: ['personName']
+}
+</script>
+<style scoped>
+  h2 {
+    font-size: 40px;
+    color: mediumseagreen;
+  }
+</style>
